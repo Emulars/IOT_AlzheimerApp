16f6d114d7314e9639df6ff7f93b527082d012a5
CelesteBazzi <73994364+CelesteBazzi@users.noreply.github.com>
2022-05-05 17:08:15 +0200

    changes made

--- app/src/main/java/com/example/profile/NewProfile.java
+++ app/src/main/java/com/example/profile/NewProfile.java
@@ -2,7 +2,10 @@ package com.example.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NewProfile extends AppCompatActivity {
 
@@ -12,5 +15,14 @@ public class NewProfile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_profile);
+        Button btnInv = (Button) findViewById(R.id.btt_start);
+
+        btnInv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent openPage1 = new Intent(NewProfile.this, PgQt.class);
+                startActivity(openPage1);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/profile/PgQt.java
@@ -0,0 +1,13 @@
+package com.example.profile;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PgQt extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qt);
+    }
+}
