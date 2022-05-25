cb5120e666c6826258be2ff91c58a996b7cf5f4d
Emulars <davide.miggiano.majo@gmail.com>
2022-05-25 15:54:56 +0200

    CLient-Server

---
+++ ML/client.py
@@ -0,0 +1,22 @@
+import requests
+
+# server url
+URL = "http://127.0.0.1:5000/predict"
+
+
+# audio file we'd like to send for predicting keyword
+FILE_PATH = "example_FR.wav"
+
+
+if __name__ == "__main__":
+
+    # open files
+    file = open(FILE_PATH, "rb")
+
+    # package stuff to send and perform POST request
+    values = {"file": (FILE_PATH, file, "audio/wav")}
+    response = requests.post(URL, files=values)
+    data = response.json()
+
+    print(values)
+    print("Predicted keyword: {}".format(data["keyword"]))
